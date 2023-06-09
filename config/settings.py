17f85f8dba21e7149cd3ed7bfde5c5d042651ea5
kabuto64425 <higasumi52@yahoo.co.jp>
2023-06-09 21:37:25 +0900

    statis_rootを追加

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # カスタムユーザーモデルの使用
 # https://docs.djangoproject.com/ja/2.1/topics/auth/customizing/#substituting-a-custom-user-model
